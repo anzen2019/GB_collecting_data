97aefdf21690cb4de014991016c8cb4bc08f0c49
alex_zen <zensanches@mail.ru>
2022-07-05 14:54:51 +0300

    сделал 2 варианта решения

--- lesson1/task_1_1.py
+++ lesson1/task_1_1.py
@@ -2,62 +2,40 @@
 1. Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев
 для конкретного пользователя, сохранить JSON-вывод в файле *.json.
 """
-# e5e4cd692a72b0b66ea0a6b80255d1c3
-# import requests
-# from pprint import pprint
-#
-# headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
-# params = {'q': 'Chelyabinsk',
-#           'appid': 'e5e4cd692a72b0b66ea0a6b80255d1c3'}
-# url = 'https://api.openweathermap.org/data/2.5/weather'
-#
-# response = requests.get(url, headers=headers, params=params)
-# j_data = response.json()
-#
-# pprint(f"Р’ РіРѕСЂРѕРґРµ {j_data.get('name')} С‚РµРјРїРµСЂР°С‚СѓСЂР° {round(j_data.get('main').get('temp') - 273.15, 2)} РіСЂР°РґСѓСЃРѕРІ")
-
 import requests
-# import os
 from pprint import pprint
-"""
-Мой токен
-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
-"""
-#как на уроке
+"""Вариант 1"""
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'}
-# params = { 'user_login' : 'johnplakon',
+# params = { 'name' : 'johnplakon',
 #             'appid' : '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'
 #            }
-
-url = "https://api.github.com/user/repos"
-username = 'johnplakon'
+username = 'alexandrei64'
 token = "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"
-repos = requests.get(url, headers=headers, auth=(username, token))
-r = requests.get('https://api.github.com/user', auth=(username, token))
-with open('repo_list.json', 'w', encoding='UTF-8') as file:
+repos = requests.get('https://api.github.com/users/'+username+'/repos', headers=headers, auth=(username, token))
+#когда писал params = params - выдавалась ошибка аунтификации... почему тут не срабатывает params?
+j_data = repos.json()
+# pprint(j_data) #вывод всего - работает
+# pprint(j_data.get('html_url')) #почему-то не работает
+# pprint(j_data["html_url"]) #тоже не работает
+with open('repo_list1.json', 'w', encoding='UTF-8') as file:
     for repo in repos.json():
         if not repo['private']:
             pprint(repo['html_url'])
             text = [repo['html_url'], '\n']
             file.writelines(text)
 
-"""
-Черновик
-#как на уроке
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'}
-# params = { 'user_login' : 'johnplakon',
-#             'appid' : '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'
-#            }
 
-url = "https://api.github.com/user/repos"
-username = 'johnplakon'
-token = "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"
-repos = requests.get(url, headers=headers, auth=(username, token))
-r = requests.get('https://api.github.com/user', auth=(username, token))
-with open('repo_list.json', 'w', encoding='UTF-8') as file:
-    for repo in repos.json():
-        if not repo['private']:
-            pprint(repo['html_url'])
-            text = [repo['html_url'], '\n']
-            file.writelines(text)
-"""
+"""Вариант 2"""
+username = input("Введите имя пользователя github: ")
+repo = requests.get('https://api.github.com/users/'+username+'/repos')
+j_data = repo.json()
+with open('repo_list2.json', 'w', encoding='UTF-8') as file:
+    for i in range(0,len(j_data)):
+      project_name = j_data[i]['name']
+      project_url = j_data[i]['svn_url']
+      data = [f'Имя проекта: {project_name}, "\n"\
+Ссылка на проект: {project_url}', '\n', '\n']
+      print(project_name, project_url)
+      file.writelines(data)
+
+
