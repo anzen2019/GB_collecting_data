17e19e8fc3fa0cc058cf9b028fc43cfcf2c8c3a1
alex_zen <zensanches@mail.ru>
2022-07-05 13:29:17 +0300

    сделал копию кода

--- lesson1/task_1_1.py
+++ lesson1/task_1_1.py
@@ -23,22 +23,41 @@ from pprint import pprint
 Мой токен
 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
 """
-#мой код
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'}
-# params = { 'user_login' : 'anzen2019',
-#            'appid' : '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'
-#
-# }
+#как на уроке
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'}
+# params = { 'user_login' : 'johnplakon',
+#             'appid' : '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'
+#            }
 
-import os
-username = 'natenka'
-# token = os.environ.get("GITHUB_TOKEN")
+url = "https://api.github.com/user/repos"
+username = 'johnplakon'
 token = "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"
-repos = requests.get('https://api.github.com/user/repos', auth=(username, token))
+repos = requests.get(url, headers=headers, auth=(username, token))
 r = requests.get('https://api.github.com/user', auth=(username, token))
 with open('repo_list.json', 'w', encoding='UTF-8') as file:
     for repo in repos.json():
         if not repo['private']:
-            print(repo['html_url'])
+            pprint(repo['html_url'])
             text = [repo['html_url'], '\n']
             file.writelines(text)
+
+"""
+Черновик
+#как на уроке
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'}
+# params = { 'user_login' : 'johnplakon',
+#             'appid' : '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'
+#            }
+
+url = "https://api.github.com/user/repos"
+username = 'johnplakon'
+token = "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"
+repos = requests.get(url, headers=headers, auth=(username, token))
+r = requests.get('https://api.github.com/user', auth=(username, token))
+with open('repo_list.json', 'w', encoding='UTF-8') as file:
+    for repo in repos.json():
+        if not repo['private']:
+            pprint(repo['html_url'])
+            text = [repo['html_url'], '\n']
+            file.writelines(text)
+"""
